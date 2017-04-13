0affe34e91cb84ab448dcd1324154037c0296ec7
jaehyukpyon <jaehyuk.pyon@gmail.com>
2017-04-13 21:30:07 +0900

    Add files via upload

---
+++ ct_201710932/changeturtletomyface.py
@@ -0,0 +1,31 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 17:54:52) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> t1.turtle.Turtle()
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    t1.turtle.Turtle()
+NameError: name 't1' is not defined
+>>> t1=turtle.Turtle()
+>>> wn.addshape("C:\\Users\\Jaehyuk\\Downloads\\jaehyukphoto1.gif")
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    wn.addshape("C:\\Users\\Jaehyuk\\Downloads\\jaehyukphoto1.gif")
+NameError: name 'wn' is not defined
+>>> wn.addshape("C:\\Users\\Jaehyuk\\Downloads\\jaephoto1.gif")
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    wn.addshape("C:\\Users\\Jaehyuk\\Downloads\\jaephoto1.gif")
+NameError: name 'wn' is not defined
+>>> wn.addshape("C:\\Users\\Jaehyuk\\Downloads\\jaephoto1.gif")
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    wn.addshape("C:\\Users\\Jaehyuk\\Downloads\\jaephoto1.gif")
+NameError: name 'wn' is not defined
+>>> wn=turtle.Screen()
+>>> wn.addshape("C:\\Users\\Jaehyuk\\Downloads\\jaephoto1.gif")
+>>> t1.shape(C:\Users\Jaehyuk\Downloads\
+	 
+SyntaxError: invalid syntax
+>>> t1.shape("C:\\Users\\Jaehyuk\\Downloads\\jaephoto1.gif")
+>>> 
